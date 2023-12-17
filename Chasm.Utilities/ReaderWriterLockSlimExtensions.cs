ea0eed73af9cd31e9ffa8e7244eb1c65b8f3978b
Abbysssal <hillankeyxl2@yandex.ru>
2023-12-17 20:14:05 +0700

    ✨ Add ReaderWriterLockSlim extensions

---
+++ Chasm.Utilities/ReaderWriterLockSlimExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+
+namespace Chasm.Utilities
+{
+    /// <summary>
+    ///   <para>Provides a set of extension methods for the <see cref="ReaderWriterLockSlim"/> class.</para>
+    /// </summary>
+    public static class ReaderWriterLockSlimExtensions
+    {
+        /// <summary>
+        ///   <para>Tries to enter the lock in read mode, and returns a <see cref="DelegateDisposable"/>, that exits read mode when disposed.</para>
+        /// </summary>
+        /// <param name="rwl">The <see cref="ReaderWriterLockSlim"/> instance.</param>
+        /// <returns>A <see cref="DelegateDisposable"/>, that exits read mode when disposed.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="rwl"/> is <see langword="null"/>.</exception>
+        /// <inheritdoc cref="ReaderWriterLockSlim.EnterReadLock" path="exception"/>
+        public static DelegateDisposable WithReaderLock(this ReaderWriterLockSlim rwl)
+        {
+            if (rwl is null) throw new ArgumentNullException(nameof(rwl));
+            return DelegateDisposable.Create(rwl.EnterReadLock, rwl.ExitReadLock);
+        }
+        /// <summary>
+        ///   <para>Tries to enter the lock in upgradeable mode, and returns a <see cref="DelegateDisposable"/>, that exits upgradeable mode when disposed.</para>
+        /// </summary>
+        /// <param name="rwl">The <see cref="ReaderWriterLockSlim"/> instance.</param>
+        /// <returns>A <see cref="DelegateDisposable"/>, that exits upgradeable mode when disposed.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="rwl"/> is <see langword="null"/>.</exception>
+        /// <inheritdoc cref="ReaderWriterLockSlim.EnterUpgradeableReadLock" path="exception"/>
+        public static DelegateDisposable WithUpgradeableReaderLock(this ReaderWriterLockSlim rwl)
+        {
+            if (rwl is null) throw new ArgumentNullException(nameof(rwl));
+            return DelegateDisposable.Create(rwl.EnterUpgradeableReadLock, rwl.ExitUpgradeableReadLock);
+        }
+        /// <summary>
+        ///   <para>Tries to enter the lock in write mode, and returns a <see cref="DelegateDisposable"/>, that exits write mode when disposed.</para>
+        /// </summary>
+        /// <param name="rwl">The <see cref="ReaderWriterLockSlim"/> instance.</param>
+        /// <returns>A <see cref="DelegateDisposable"/>, that exits write mode when disposed.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="rwl"/> is <see langword="null"/>.</exception>
+        /// <inheritdoc cref="ReaderWriterLockSlim.EnterWriteLock" path="exception"/>
+        public static DelegateDisposable WithWriterLock(this ReaderWriterLockSlim rwl)
+        {
+            if (rwl is null) throw new ArgumentNullException(nameof(rwl));
+            return DelegateDisposable.Create(rwl.EnterWriteLock, rwl.ExitWriteLock);
+        }
+
+    }
+}
