8d2c436723e3ab18376f6f6f2106d4c801298cb8
Abbysssal <hillankeyxl2@yandex.ru>
2023-12-17 18:03:21 +0700

    🧩 Retarget Chasm.Utilities for lower targets

---
+++ Chasm.Utilities/_Shim.cs
@@ -0,0 +1,8 @@
+﻿#if !(NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER)
+// ReSharper disable once CheckNamespace
+namespace System.Diagnostics.CodeAnalysis
+{
+    #pragma warning disable CS9113 // Parameter is unread.
+    internal class NotNullWhenAttribute(bool _) : Attribute;
+}
+#endif
