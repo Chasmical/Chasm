b5f1feab0272efb4b59ca612dd85183a410d7050
Abbysssal <hillankeyxl2@yandex.ru>
2024-01-02 14:56:34 +0700

    ✅ Add SemanticVersion comparison tests

---
+++ Chasm.SemanticVersioning.Tests/SemanticVersion.Comparison.Fixtures.cs
@@ -0,0 +1,45 @@
+﻿using Chasm.Collections;
+using JetBrains.Annotations;
+
+namespace Chasm.SemanticVersioning.Tests
+{
+    public partial class SemanticVersionTests
+    {
+        [Pure] public static SemanticVersion[] CreateComparisonFixtures()
+        {
+            string[] sources =
+            [
+                // 0.0.0-0 - SemanticVersion.MinValue
+                "0.0.0-0.0",
+                "0.0.0--",
+                "0.0.0-a",
+                "0.0.1",
+                "0.0.4",
+                "0.1.0",
+                "0.1.9",
+                "0.5.0",
+                "1.0.0",
+                "1.2.3-a",
+                "1.2.3-al",
+                "1.2.3-al.beta",
+                "1.2.3-al-beta",
+                "1.2.3-alpha",
+                "1.2.3-beta",
+                "1.2.3-beta.0",
+                "1.2.3-beta.91",
+                "1.2.3-beta.456",
+                "1.2.3-beta.gamma",
+                "1.2.3",
+                "4.5.6-0",
+                "4.5.6-pre.0",
+                "4.5.6",
+                "99.99.99-0",
+                "99.99.99",
+                // 2147483647.2147483647.2147483647 - SemanticVersion.MaxValue
+            ];
+            SemanticVersion[] versions = sources.ConvertAll(SemanticVersion.Parse);
+
+            return [SemanticVersion.MinValue, ..versions, SemanticVersion.MaxValue];
+        }
+    }
+}
---
+++ Chasm.SemanticVersioning.Tests/SemanticVersion.Comparison.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Xunit;
+// ReSharper disable SuspiciousTypeConversion.Global
+
+namespace Chasm.SemanticVersioning.Tests
+{
+    public partial class SemanticVersionTests
+    {
+        [Fact]
+        public void Comparison()
+        {
+            SemanticVersion[] fixtures1 = CreateComparisonFixtures();
+            SemanticVersion[] fixtures2 = CreateComparisonFixtures();
+            SemanticVersion? a = default, b = default;
+
+            try
+            {
+                for (int i = 0; i < fixtures1.Length; i++)
+                {
+                    a = fixtures1[i];
+
+                    // Test Equals and CompareTo against null
+                    Assert.False(((object)a).Equals(null));
+                    Assert.Equal(1, ((IComparable)a).CompareTo(null));
+
+                    // Make sure they don't work with objects of other types
+                    Assert.False(a.Equals("0"));
+                    Assert.False(a.Equals(0));
+                    Assert.Throws<ArgumentException>(() => ((IComparable)a).CompareTo("0"));
+                    Assert.Throws<ArgumentException>(() => ((IComparable)a).CompareTo(0));
+
+                    // Test against other versions
+                    for (int j = 0; j < fixtures2.Length; j++)
+                    {
+                        b = fixtures2[j];
+
+                        // Test Equals and CompareTo implementations
+                        Assert.Equal(i.Equals(j), a.Equals(b));
+                        Assert.Equal(i.Equals(j), ((object)a).Equals(b));
+                        // As specified by IComparable, CompareTo doesn't necessarily return -1 or 1 on inequality
+                        Assert.Equal(i.CompareTo(j), Math.Sign(a.CompareTo(b)));
+                        Assert.Equal(i.CompareTo(j), Math.Sign(((IComparable)a).CompareTo(b)));
+                        // Make sure the hash code is consistent
+                        Assert.Equal(i == j, a.GetHashCode() == b.GetHashCode());
+
+                        // Test overloaded operators
+                        Assert.Equal(i == j, a == b);
+                        Assert.Equal(i != j, a != b);
+                        Assert.Equal(i > j, a > b);
+                        Assert.Equal(i < j, a < b);
+                        Assert.Equal(i >= j, a >= b);
+                        Assert.Equal(i <= j, a <= b);
+
+                    }
+                }
+            }
+            catch
+            {
+                Output.WriteLine($"Error comparing {a} with {b}");
+                throw;
+            }
+        }
+    }
+}
