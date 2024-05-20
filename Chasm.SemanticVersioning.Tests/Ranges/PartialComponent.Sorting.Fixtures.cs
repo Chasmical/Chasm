d4b410d2a53d0f0a0cef0b67d619b62c9fbd0d9e
Abbysssal <hillankeyxl2@yandex.ru>
2024-05-20 22:25:42 +0700

    ✅ Add PartialComponent sorting test

---
+++ Chasm.SemanticVersioning.Tests/Ranges/PartialComponent.Sorting.Fixtures.cs
@@ -0,0 +1,23 @@
+﻿using Chasm.SemanticVersioning.Ranges;
+using JetBrains.Annotations;
+
+namespace Chasm.SemanticVersioning.Tests
+{
+    public partial class PartialComponentTests
+    {
+        [Pure] public static PartialComponent[][] CreateSortingFixtures()
+        {
+            return [
+                ['x', 'X', '*', null],
+                [0],
+                [1],
+                [2],
+                [24],
+                [42],
+                [201],
+                [19812322],
+                [int.MaxValue],
+            ];
+        }
+    }
+}
