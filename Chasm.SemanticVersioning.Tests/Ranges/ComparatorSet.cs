210ca2ed38c127af7778688764a64303c5d70d47
Abbysssal <hillankeyxl2@yandex.ru>
2024-06-03 02:00:02 +0700

    ✅ Add comparator set tests

---
+++ Chasm.SemanticVersioning.Tests/Ranges/ComparatorSet.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using Chasm.SemanticVersioning.Ranges;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Chasm.SemanticVersioning.Tests
+{
+    public partial class ComparatorSetTests(ITestOutputHelper output)
+    {
+        public ITestOutputHelper Output { get; } = output;
+
+        [Fact]
+        public void ConstructorsArguments()
+        {
+            ArgumentException ex = Assert.Throws<ArgumentException>(static () => new ComparatorSet([null!]));
+            Assert.StartsWith(Exceptions.ComparatorsNull, ex.Message);
+            ex = Assert.Throws<ArgumentException>(static () => new ComparatorSet(new List<Comparator> { null! }));
+            Assert.StartsWith(Exceptions.ComparatorsNull, ex.Message);
+        }
+
+        [Fact]
+        public void Constructors()
+        {
+            PrimitiveComparator a = PrimitiveComparator.GreaterThanOrEqual(new SemanticVersion(1, 2, 3));
+            PrimitiveComparator b = PrimitiveComparator.LessThan(new SemanticVersion(2, 0, 0, [0]));
+
+            // test single comparator constructor
+            ComparatorSet set = new ComparatorSet(a);
+            Assert.Same(a, Assert.Single(set.Comparators));
+
+            // test params comparators constructor (empty params)
+            set = new ComparatorSet([]);
+            Assert.Empty(set.Comparators);
+
+            // test params comparators constructor
+            set = new ComparatorSet(a, b);
+            Assert.Collection(
+                set.Comparators,
+                cs => Assert.Same(a, cs),
+                cs => Assert.Same(b, cs)
+            );
+
+            // test IEnumerable comparators constructor
+            set = new ComparatorSet(new List<Comparator> { a, b });
+            Assert.Collection(
+                set.Comparators,
+                cs => Assert.Same(a, cs),
+                cs => Assert.Same(b, cs)
+            );
+
+        }
+
+        [Fact]
+        public void Properties()
+        {
+            ComparatorSet set = VersionRange.Parse(">3.5.x || <2.0.0-0 >=1.3.0").ComparatorSets[0];
+
+            // test comparator collections
+            Assert.Equal(set._comparators, set.Comparators);
+            Assert.Equal(set._comparators, set.GetComparators().ToArray());
+
+            // make sure the read-only collection is memoized
+            Assert.Same(set.Comparators, set.Comparators);
+
+        }
+
+        [Fact]
+        public void StaticProperties()
+        {
+            // make sure that properties return correct values
+            Assert.Equal("<0.0.0-0", ComparatorSet.None.ToString());
+            Assert.Equal("*", ComparatorSet.All.ToString());
+
+            // make sure that properties aren't constantly creating new instances
+            Assert.Same(ComparatorSet.None, ComparatorSet.None);
+            Assert.Same(ComparatorSet.All, ComparatorSet.All);
+        }
+
+    }
+}
