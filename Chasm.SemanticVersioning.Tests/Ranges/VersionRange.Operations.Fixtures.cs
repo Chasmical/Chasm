53cb5cdd5aa20ae170ec6c29b3e3cfed345ae55b
Chasmical <hillankeyxl2@yandex.ru>
2024-07-09 18:27:14 +0700

    ✅ Add basic intersection and union tests

---
+++ Chasm.SemanticVersioning.Tests/Ranges/VersionRange.Operations.Fixtures.cs
@@ -0,0 +1,96 @@
+﻿using Chasm.SemanticVersioning.Ranges;
+using System;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace Chasm.SemanticVersioning.Tests
+{
+    public partial class VersionRangeTests
+    {
+        [Pure] public static FixtureAdapter<OperationFixture> CreateOperationFixtures()
+        {
+            FixtureAdapter<OperationFixture> adapter = [];
+
+            OperationFixture New(string left, char op, string? right = null)
+                => adapter.Add(new OperationFixture(op, left, right));
+
+
+
+            // Intersection of primitives, same direction
+            New(">1.2.3", '&', ">3.4.5").Returns(">3.4.5", true);
+            New("<1.2.3", '&', "<3.4.5").Returns("<1.2.3", true);
+            New("<=1.2.3", '&', "<1.2.3").Returns("<1.2.3", true);
+            New(">=1.2.3", '&', ">1.2.3").Returns(">1.2.3", true);
+
+            // Intersection of primitives, opposite directions
+            New("<1.2.3", '&', ">3.4.5").Returns("<0.0.0-0", true);
+            New(">1.2.3", '&', "<3.4.5").Returns(">1.2.3 <3.4.5");
+            New("<3.4.5", '&', ">1.2.3").Returns("<3.4.5 >1.2.3");
+
+            New("<1.2.3", '&', ">1.2.3").Returns("<0.0.0-0", true);
+            New("<=1.2.3", '&', ">1.2.3").Returns("<0.0.0-0", true);
+            New(">=1.2.3", '&', "<1.2.3").Returns("<0.0.0-0", true);
+            New("<=1.2.3", '&', ">=1.2.3").Returns("=1.2.3", true);
+
+            // Intersection of primitives, with minimum version
+            New("<0.0.0-0", '&', "<0.0.0-0").Returns("<0.0.0-0", true);
+            New("<0.0.0-0", '&', "<0.0.0").Returns("<0.0.0-0", true);
+            New("<0.0.0-0", '&', "<=0.0.0-0").Returns("<0.0.0-0", true);
+            New("<0.0.0-0", '&', ">0.0.0-0").Returns("<0.0.0-0", true);
+
+
+
+            // Union of primitives, same direction
+            New(">1.2.3", '|', ">3.4.5").Returns(">1.2.3", true);
+            New("<1.2.3", '|', "<3.4.5").Returns("<3.4.5", true);
+            New(">=1.2.3", '|', ">1.2.3").Returns(">=1.2.3", true);
+            New("<=1.2.3", '|', "<1.2.3").Returns("<=1.2.3", true);
+
+            // Union of primitives, opposite directions
+            New(">1.2.3", '|', "<3.4.5").Returns("*", true);
+            New("<1.2.3", '|', ">3.4.5").Returns("<1.2.3 || >3.4.5");
+            New(">3.4.5", '|', "<1.2.3").Returns(">3.4.5 || <1.2.3");
+
+            New("<1.2.3", '|', ">1.2.3").Returns("<1.2.3 || >1.2.3");
+            New(">1.2.3", '|', "<1.2.3").Returns(">1.2.3 || <1.2.3");
+            New("<=1.2.3", '|', ">1.2.3").Returns("*", true);
+            New("<1.2.3", '|', ">=1.2.3").Returns("*", true);
+            New("<=1.2.3", '|', ">=1.2.3").Returns("*", true);
+
+            // Union of primitives, with minimum version
+            New("<0.0.0-0", '|', "<0.0.0-0").Returns("<0.0.0-0", true);
+            New("<0.0.0-0", '|', "<0.0.0").Returns("<0.0.0", true);
+            New("<0.0.0-0", '|', ">=0.0.0-0").Returns("*", true);
+            New("<=0.0.0-0", '|', ">0.0.0-0").Returns("*", true);
+
+
+
+            return adapter;
+        }
+
+        public sealed class OperationFixture(char operation, string left, string? right = null) : FuncFixture<VersionRange>
+        {
+            [Obsolete(TestUtil.DeserCtor, true)] public OperationFixture() : this(default, null!) { }
+
+            public char Operation { get; } = operation;
+            public string Left { get; } = left;
+            public string? Right { get; } = right;
+
+            public string? Expected { get; private set; }
+
+            public void Returns(string expected, bool isCommutative = false)
+            {
+                MarkAsComplete();
+                Expected = expected;
+
+                if (isCommutative) AddNew(new OperationFixture(Operation, Right!, Left)).Returns(expected);
+            }
+
+            public override void AssertResult(VersionRange? result)
+                => Assert.Equal(Expected, result?.ToString());
+            public override string ToString()
+                => $"{base.ToString()} {(Right is null ? $"{Operation} {Left}" : $"{Left} {Operation} {Right} ⇒ {Expected}")}";
+
+        }
+    }
+}
---
+++ Chasm.SemanticVersioning.Tests/Ranges/VersionRange.Operations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Chasm.SemanticVersioning.Ranges;
+using Xunit;
+
+namespace Chasm.SemanticVersioning.Tests
+{
+    public partial class VersionRangeTests
+    {
+        [Theory, MemberData(nameof(CreateOperationFixtures))]
+        public void Operations(OperationFixture fixture)
+        {
+            VersionRange leftRange = VersionRange.Parse(fixture.Left);
+            _ = VersionRange.TryParse(fixture.Right, out VersionRange? rightRange);
+
+            // TODO: at the moment, only operations with one comparator in each operand are tested
+
+            Comparator left = Assert.Single(Assert.Single(leftRange.ComparatorSets).Comparators);
+            Comparator? right = rightRange is null ? null : Assert.Single(Assert.Single(rightRange.ComparatorSets).Comparators);
+
+            switch (fixture.Operation)
+            {
+                case '~':
+                    //fixture.Test(() => ~left);
+                    break;
+                case '&':
+                    Assert.NotNull(right);
+                    fixture.Test(() => left & right);
+                    break;
+                case '|':
+                    Assert.NotNull(right);
+                    fixture.Test(() => left | right);
+                    break;
+                default:
+                    throw new InvalidOperationException();
+            }
+
+        }
+    }
+}
