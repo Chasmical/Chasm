9a0b1f999e46fdc841c95e2913b1562ae401582b
Chasmical <hillankeyxl2@yandex.ru>
2024-07-09 20:33:54 +0700

    ✨ Implement comparator complement operator

--- Chasm.SemanticVersioning/Ranges/Comparator.Complement.cs
+++ Chasm.SemanticVersioning/Ranges/Comparator.Complement.cs
@@ -8,27 +8,25 @@ namespace Chasm.SemanticVersioning.Ranges
         [Pure] public static VersionRange operator ~(Comparator comparator)
             => VersionRange.FromTuple(Complement(comparator));
 
-        [Pure] public static (ComparatorSet, ComparatorSet?) Complement(Comparator comparator)
+        [Pure] public static (Comparator, Comparator?) Complement(Comparator comparator)
         {
-            if (ReferenceEquals(comparator, PrimitiveComparator.None))
-                return (XRangeComparator.All, null);
-            if (ReferenceEquals(comparator, XRangeComparator.All))
-                return (PrimitiveComparator.None, null);
-
             (PrimitiveComparator? left, PrimitiveComparator? right) = comparator.AsPrimitives();
 
             if (left?.Operator.IsEQ() == true)
                 return (PrimitiveComparator.LessThan(left.Operand), PrimitiveComparator.GreaterThan(left.Operand));
 
             if (left is null)
-                return right is null ? (PrimitiveComparator.None, null) : (ComplementPrimitive(right), null);
+                return (right is null ? PrimitiveComparator.None : ComplementPrimitive(right), null);
 
             return (ComplementPrimitive(left), right is null ? null : ComplementPrimitive(right));
         }
 
-        [Pure] private static PrimitiveComparator ComplementPrimitive(PrimitiveComparator primitive)
+        [Pure] private static Comparator ComplementPrimitive(PrimitiveComparator primitive)
         {
             Debug.Assert(!primitive.Operator.IsEQ());
+
+            if (primitive.Equals(PrimitiveComparator.None)) return XRangeComparator.All;
+
             return new PrimitiveComparator(primitive.Operand, primitive.Operator.Invert());
         }
 
