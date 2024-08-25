6478465a0798c0a8135cee0b85ec38b902a6a53e
Chasmical <hillankeyxl2@yandex.ru>
2024-08-25 12:43:54 +0700

    🚨 Fix nullability analysis with Debug.Assert

--- Chasm.SemanticVersioning/Internal/Shims.cs
+++ Chasm.SemanticVersioning/Internal/Shims.cs
@@ -12,5 +12,32 @@ namespace System.Diagnostics.CodeAnalysis
     {
         public string ParameterName { get; } = parameterName;
     }
+    [AttributeUsage(AttributeTargets.Method, Inherited = false)]
+    internal sealed class DoesNotReturnAttribute : Attribute;
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    internal sealed class DoesNotReturnIfAttribute(bool parameterValue) : Attribute
+    {
+        public bool ParameterValue { get; } = parameterValue;
+    }
+}
+
+namespace Chasm.SemanticVersioning
+{
+    using System.Diagnostics;
+    using System.Diagnostics.CodeAnalysis;
+
+    // redirect the system's Debug to this one, for proper nullable analysis
+    internal static class Debug
+    {
+        [Conditional("DEBUG")]
+        public static void Assert([DoesNotReturnIf(false)] bool condition)
+            => System.Diagnostics.Debug.Assert(condition);
+
+#pragma warning disable CS8763
+        [Conditional("DEBUG"), DoesNotReturn]
+        public static void Fail(string message)
+            => System.Diagnostics.Debug.Fail(message);
+#pragma warning restore CS8763
+    }
 }
 #endif
